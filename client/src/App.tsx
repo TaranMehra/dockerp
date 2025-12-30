721de28a81657ed7b558b646ba71545ddf7c8133
TaranMehra <taranimpworks@gmail.com>
2025-12-30 10:30:49 +0530

    vps hosting with docker

---
+++ client/src/App.tsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+
+type ApiData = {
+  name: string;
+  instance: number;
+};
+function App() {
+  const [data, SetData] = useState<ApiData>();
+
+  useEffect(() => {
+    fetch("http://localhost:4000/api/user")
+      .then((res) => res.json())
+      .then((data) => SetData(data))
+      .catch((error) => {
+        return console.error("Error fetching message ", error);
+      });
+  });
+
+  return (
+    <>
+      <h1>Welcome to waheguru frontend</h1>
+      <h2 className="read-the-docs">Name : {data?.name}</h2>
+      <h2 className="read-the-docs">Instance : {data?.instance}</h2>
+    </>
+  );
+}
+
+export default App;
